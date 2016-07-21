def3b42aba797e964b1218bc91b6417e727702bb
Hao Shi <boruoshihao@gmail.com>
2016-07-21 10:33:19 -0400

    Move operator += in Kahan class to kahan.cpp

--- inc/kahan.h
+++ inc/kahan.h
@@ -1,6 +1,7 @@
 #ifndef KAHANDATA_HAO_H
 #define KAHANDATA_HAO_H
 #include <iostream>
+#include <complex>
 
 template<class T> class KahanData
 {
@@ -27,17 +28,22 @@ template<class T> class KahanData
     KahanData<T> operator  = (const KahanData<T> & x) {sum=x.sum;c=x.c;return *this;}
    
     //+=
-    KahanData<T> operator += (const T& add) 
-    {
-        //We can not use it because "volatile std::complex" does not support well
-        //volatile T  y = add-c;        // volatile here is to avoid complier's agressive optimazation
-        //volatile T  t = sum+y;        // otherwise, compiler might set c to zero directly
-                   T  y = add-c;
-                   T  t = sum+y; 
-                    c = (t-sum)-y;
-                  sum = t;
-        return *this;
-    }
+    KahanData<T> operator += (const T& add);
+
+    //Declare operator += in kahan.cpp, this will avoid agressive optimazation
+    //We can also change CMAKELIST.TXT, to only reduce optimazation flag for this library
+    //If we put in the header, we can not only optimaze this library!!!
+    //KahanData<T> operator += (const T& add) 
+    //{
+    //    //We can not use it because "volatile std::complex" does not support well
+    //    //volatile T  y = add-c;        // volatile here is to avoid complier's agressive optimazation
+    //    //volatile T  t = sum+y;        // otherwise, compiler might set c to zero directly
+    //               T  y = add-c;
+    //               T  t = sum+y; 
+    //                c = (t-sum)-y;
+    //              sum = t;
+    //    return *this;
+    //}
    
 };
 
