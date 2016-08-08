17371388f3d99c8b3b979f767132ab6d4c7e9b34
Hao Shi <boruoshihao@gmail.com>
2016-08-08 10:41:52 -0400

    clean the code.

--- bin/complex_double_error_analysis.cpp
+++ bin/complex_double_error_analysis.cpp
@@ -8,11 +8,12 @@
 #include "kahan.h"
 
 using namespace std;
+
 int main(int argc, char** argv)
 {
     if( argc<3 ) {cout<<"Error!!!!! Need input file name!"<<endl;}
-    string input  = argv[1];
-    string output = argv[2];
+    string in_filename  = argv[1];
+    string out_filename = argv[2];
 
     vector<complex<double>> vec;
     vector<size_t> fact;
@@ -21,7 +22,7 @@ int main(int argc, char** argv)
 
     //read data to vec
     ifstream input_file;
-    input_file.open(input, ios::in);
+    input_file.open(in_filename, ios::in);
     if ( ! input_file.is_open() ) {cout << "Error opening input file in double_error_analysis.cpp!!!"; exit(1);}
     double re, im;
     while (input_file >> re >> im) vec.push_back( complex<double>(re, im) );
@@ -32,7 +33,7 @@ int main(int argc, char** argv)
 
     //write data to output
     ofstream output_file;
-    output_file.open(output, ios::out|ios::trunc);
+    output_file.open(out_filename, ios::out|ios::trunc);
     if( !output_file.is_open() ) {cout << "Error opening output file in double_error_analysis.cpp!!! "<<endl; exit(1);}
     output_file<<setprecision(16)<<scientific;
     for(size_t i=0; i<fact.size(); i++) output_file<<setw(26)<<fact[i]<<setw(26)<<mean[i].real()<<setw(26)<<mean[i].imag()<<setw(26)<<err[i]<<"\n";
